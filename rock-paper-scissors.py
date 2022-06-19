e4c59e20c68c5026722d90b977302320bb6a800b
Arda Alp <82732641+ArdaAlp@users.noreply.github.com>
2022-06-19 19:20:18 +0300

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,4 +1,4 @@
-# AUTHOR:ARDA ALP
+#AUTHOR:ARDA ALP
 
 import random as rdm
 
@@ -6,17 +6,23 @@ moves = ("rock", "paper", "scissors")
 
 data = int(input("Enter Your Move Number \n1-Rock \n2-Paper \n3-Scissors \nYour Move:"))
 
-userMove = data - 1
-user = moves[userMove] 
+if data >= 4:
+  print("Move number doesn't found")
 
-compMove = rdm.randrange(0,3)
-comp = moves[compMove]
-
-if comp == "rock" and user == "scissors" or comp == "paper" and user == "rock" or comp == "scissors" and user == "paper":
-  print("----------\nComputer:", comp, "\nUser:", user, "\nComputer Won!")
-
-elif comp == user:
-  print("----------\nDraw!")
- 
 else:
-  print("----------\nComputer:", comp, "\nUser:", user, "\nUser Won!") 
+
+  userMove = data - 1
+  user = moves[userMove] 
+
+  compMove = rdm.randrange(0,3)
+  comp = moves[compMove]
+
+  if comp == "rock" and user == "scissors" or comp == "paper" and user == "rock" or comp == "scissors" and user == "paper":
+    print("----------\nComputer:", comp, "\nYou:", user, "\nCOMPUTER WON!")
+
+  elif comp == user:
+    print("----------\nDraw!")
+ 
+  else:
+    print("----------\nComputer:", comp, "\nYou:", user, "\nYOU WON!") 
+    
